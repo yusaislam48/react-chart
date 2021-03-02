79b9a1d2db099c833e53dd93edc0300c4609f77e
Yusa Islam <yusaislam48@gmail.com>
2021-03-03 02:14:10 +0600

    make chart with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import AreaChart from './Components/Chart/AreaChart';
+import Chart from './Components/Chart/Chart';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Chart></Chart>
+      <AreaChart></AreaChart>
     </div>
   );
 }
